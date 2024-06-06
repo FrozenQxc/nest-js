2b0dcaf2edd1eab116b077b7e776ff8acdf1b043
FrozenDev <125030768+FrozenQxc@users.noreply.github.com>
2024-06-06 18:38:05 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import config from '../config';
 import { UserModule } from '../user/user.module';
 import { AppController } from './app.controller';
@@ -11,6 +13,32 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [config],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): SequelizeModuleOptions => ({
+        dialect: 'postgres',
+        host: configService.get('db_host'),
+        port: configService.get('db_port'),
+        username: configService.get('db_user'),
+        password: configService.get('db_password'),
+        database: configService.get('db_name'),
+
+        synchronize: true,
+        autoLoadModels: true,
+        models: [],
+      }),
     }),
   ],
   controllers: [AppController],
